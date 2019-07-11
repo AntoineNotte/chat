776962bda57f4ebf0505a6086497dc5d1f8ac505
Maxim Lopez <maxim.lopez.02@gmail.com>
2019-07-11 16:06:23 +0200

    11-07-19  - 16.06
    Add view

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const app = require('express')(); 
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+//https://socket.io/get-started/chat/
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.get ('/', function(req, res){ 
-  
-    res.sendFile('index.html', { root : './'});
-  });
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-  io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
     });
-  });
+});
 
-  io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
     });
-  });
+});
 
-http.listen(3000, function(){console.log('listening on *: 3000');});
-  
-
+io.emit('some event', {
+    for: 'everyone'
+});
+io.on('connection', function (socket) {
+    socket.broadcast.emit('hi');
+});
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});

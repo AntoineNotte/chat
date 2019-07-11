a5ede4de6941cbc50c24a041c3cedc38462e1eb9
Dejaiffe Christophe <cdejaiffe@gmail.com>
2019-07-11 16:30:22 +0200

    ajout remarque

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //https://socket.io/get-started/chat/
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
@@ -7,18 +8,14 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
+io.on('connection', function (socket) { // gere les connections
+    console.log('User connected');
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        console.log('User disconnected');
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
-
-io.on('connection', function (socket) {
+io.on('connection', function (socket) { // affiche les messages dans la console
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
     });
@@ -34,4 +31,7 @@ io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
+});
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
